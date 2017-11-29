17d19a4e9297f42f4695d53c9db700d71c7a00a9
beelarr <bryonl@me.com>
2017-11-28 23:28:00 -0600

    Finished proof of concept

---
+++ README.md
@@ -0,0 +1,13 @@
+## Outdoor Movie
+
+
+#### Proof of concept of embedding audio and a video file in React VR.
+
+* React VR
+* Audacity for making background audio
+* For demonstration purposes only
+
+```angular2html
+npm install
+npm start
+```
--- vr/Components/Scenes/Layouts/Elements/MovieTitle.js
+++ vr/Components/Scenes/Layouts/Elements/MovieTitle.js
@@ -6,17 +6,20 @@ import { Easing } from 'react-native';
 export default class MovieTitle extends React.Component {
     constructor() {
         super();
-        this.state = {slideValue: new Animated.Value(1.5)}
+        this.state = {
+            fadeSlide: new Animated.Value(0)
+        }
     }
 
 
 componentDidMount() {
-        Animated.spring(
-          this.state.slideValue,
+        Animated.timing(
+          this.state.fadeSlide,
             {
-                toValue: 0,
-                friction: 2,
-                tension: 5
+                toValue:1,
+                duration: 1000,
+                delay: 100,
+                easing: Easing.ease
             }
         ).start();
 }
@@ -30,10 +33,15 @@ componentDidMount() {
                 style={{
                     fontSize: 0.5,
                     fontWeight: '400',
+                    opacity: this.state.fadeSlide,
                     textAlign: 'center',
                     textAlignVertical: 'center',
                     transform: [
-                        {translateY: this.state.slideValue}
+                        {translateY: this.state.fadeSlide.interpolate({
+                            inputRange: [0, 1],
+                            outputRange: [.3, 0]
+                        })
+                        }
                     ]
 
                 }}
